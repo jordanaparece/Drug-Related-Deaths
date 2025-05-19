31d7258138d5d80027db039a76db4ac62c90038c
jordanaparece <168220393+jordanaparece@users.noreply.github.com>
2025-05-18 22:29:31 -0400

    Add files via upload

---
+++ Data Cleaning.sql
@@ -0,0 +1,171 @@
+UPDATE drug_deaths
+SET ethnicity = 'Other Spanish/Hispanic/Latino'
+WHERE ethnicity IS NULL
+
+UPDATE drug_deaths
+SET residence_city = 'NEW HAVEN'
+WHERE residence_city IS NULL
+
+UPDATE drug_deaths
+SET residence_county = 'NEW HAVEN'
+WHERE residence_county IS NULL
+
+UPDATE drug_deaths
+SET residence_state = 'CT'
+WHERE residence_state IS NULL
+
+UPDATE drug_deaths
+SET injury_city = 'NEW HAVEN'
+WHERE injury_city IS NULL
+
+UPDATE drug_deaths
+SET injury_county = 'NEW HAVEN'
+WHERE injury_county IS NULL
+
+UPDATE drug_deaths
+SET injury_state = 'CT'
+WHERE injury_state IS NULL
+
+UPDATE drug_deaths
+SET injury_place = 'Unknown'
+WHERE injury_place IS NULL
+
+UPDATE drug_deaths
+SET description_of_injury = 'Substance use'
+WHERE description_of_injury IS NULL
+
+UPDATE drug_deaths
+SET death_city = 'HARTFORD'
+WHERE death_city IS NULL
+
+UPDATE drug_deaths
+SET death_county = 'NEW HAVEN'
+WHERE death_county IS NULL
+
+UPDATE drug_deaths
+SET death_state = 'CT'
+WHERE death_state IS NULL
+
+UPDATE drug_deaths
+SET location = 'Other'
+WHERE location IS NULL
+
+UPDATE drug_deaths
+SET location_if_other = 'Other'
+WHERE location_if_other IS NULL
+
+UPDATE drug_deaths
+SET cause_of_death = 'Other'
+WHERE cause_of_death IS NULL
+
+UPDATE drug_deaths
+SET manner_of_death = 'Unknown'
+WHERE manner_of_death IS NULL
+
+UPDATE drug_deaths
+SET other_significant_conditions = 'Other'
+WHERE other_significant_conditions IS NULL
+
+UPDATE drug_deaths
+SET heroin = 'N'
+WHERE heroin IS NULL
+
+UPDATE drug_deaths
+SET heroin_death_certificate_dc = 'N'
+WHERE heroin_death_certificate_dc IS NULL
+
+UPDATE drug_deaths
+SET cocaine = 'N'
+WHERE cocaine IS NULL
+
+UPDATE drug_deaths
+SET fentanyl = 'N'
+WHERE fentanyl IS NULL
+
+UPDATE drug_deaths
+SET fentanyl_analogue = 'N'
+WHERE fentanyl_analogue IS NULL
+
+UPDATE drug_deaths
+SET oxycodone = 'N'
+WHERE oxycodone IS NULL
+
+UPDATE drug_deaths
+SET oxymorphone = 'N'
+WHERE oxymorphone IS NULL
+
+UPDATE drug_deaths
+SET ethanol = 'N'
+WHERE ethanol IS NULL
+
+UPDATE drug_deaths
+SET hydrocodone = 'N'
+WHERE hydrocodone IS NULL
+
+UPDATE drug_deaths
+SET benzodiazepine = 'N'
+WHERE benzodiazepine IS NULL
+
+UPDATE drug_deaths
+SET methadone = 'N'
+WHERE methadone IS NULL
+
+UPDATE drug_deaths
+SET methamphetamine = 'N'
+WHERE methamphetamine IS NULL
+
+UPDATE drug_deaths
+SET amphet = 'N'
+WHERE amphet IS NULL
+
+UPDATE drug_deaths
+SET tramad = 'N'
+WHERE tramad IS NULL
+
+UPDATE drug_deaths
+SET hydromorphone = 'N'
+WHERE hydromorphone IS NULL
+
+UPDATE drug_deaths
+SET morphine_not_heroin = 'N'
+WHERE morphine_not_heroin IS NULL
+
+UPDATE drug_deaths
+SET xylazine = 'N'
+WHERE xylazine IS NULL
+
+UPDATE drug_deaths
+SET gabapentin = 'N'
+WHERE gabapentin IS NULL
+
+UPDATE drug_deaths
+SET opiate_nos = 'N'
+WHERE opiate_nos IS NULL
+
+UPDATE drug_deaths
+SET heroin_morph_codeine = 'N'
+WHERE heroin_morph_codeine IS NULL
+
+UPDATE drug_deaths
+SET other_opioid = 'N'
+WHERE other_opioid IS NULL
+
+UPDATE drug_deaths
+SET any_opioid = 'N'
+WHERE any_opioid IS NULL
+
+UPDATE drug_deaths
+SET other = 'Unknown'
+WHERE other IS NULL
+
+UPDATE drug_deaths
+SET residencecitygeo = 'Unknown'
+WHERE residencecitygeo IS NULL
+
+UPDATE drug_deaths
+SET injurycitygeo = 'Unknown'
+WHERE injurycitygeo IS NULL
+
+UPDATE drug_deaths
+SET deathcitygeo = 'Unknown'
+WHERE deathcitygeo IS NULL
